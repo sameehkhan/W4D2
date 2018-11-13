9bd26325943257fc1bf475681e8f75f8aa06ac40
sameehkhan <sameehkhan1@gmail.com>
2018-11-13 14:26:39 -0800

    Add validations to cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -14,6 +14,14 @@
 
 
 class Cat < ApplicationRecord
+validates :name, :birth_date, :color, :sex, :description, presence: true 
+
+validates :color, inclusion: { in: %w(red white black blue orange green yellow purple),
+    message: "%{value} is not a valid color" }
+
+validates :sex, inclusion: { in: %w(M F),
+      message: "Please enter valid option (i.e. 'F' )" }
+
   include ActionView::Helpers::DateHelper
 
   def age
