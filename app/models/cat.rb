153b81ec1378253ef4053a3564414b53b19a6d38
sameehkhan <sameehkhan1@gmail.com>
2018-11-13 14:21:02 -0800

    Add age method to Cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint(8)        not null, primary key
+#  birth_date  :string           not null
+#  color       :string
+#  name        :string           not null
+#  sex         :string(1)
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  def age
+    time_ago_in_words(birth_date)
+  end
 end
--- test/models/cat_test.rb
+++ test/models/cat_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint(8)        not null, primary key
+#  birth_date  :string           not null
+#  color       :string
+#  name        :string           not null
+#  sex         :string(1)
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class CatTest < ActiveSupport::TestCase
